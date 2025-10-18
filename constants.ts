fceb68e217a527115d7b02320a2a53c827e8e12d
ravi-rtb <138711859+ravi-rtb@users.noreply.github.com>
2025-10-18 16:37:10 +0530

    Update APPS_SCRIPT_URL to new deployment URL
    For parsing date correctly while editing the cause of failure.

--- constants.ts
+++ constants.ts
@@ -3,7 +3,7 @@
 export const GOOGLE_CLIENT_ID = '999272865261-b3vf4mvie07h0ag72taqc2dnatihjhfd.apps.googleusercontent.com';
 
 // IMPORTANT: Replace with the Web app URL you get after deploying your Google Apps Script.
-export const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzBXADjMp4hKmI2XZ3YowTB1moZRulWC916TTQONIVEsO3-99ccjaVMpk8QOAlOiSZS/exec';
+export const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxJuBlj7U5xofUPRBGWX8DaBQv2NrfQS8Q4pyXju8V81X4RZrwNZsg4CZkoN_-mnEK-/exec';
 
 // The unique ID of your Google Sheet.
 // Found in the URL: https://docs.google.com/spreadsheets/d/SPREADSHEET_ID/edit
